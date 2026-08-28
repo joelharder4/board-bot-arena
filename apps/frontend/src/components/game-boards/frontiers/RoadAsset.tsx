69c4d205b99d2c4c26c3d9a5d20b85b54fce61a7
TheAngryPopo <joelharder4@gmail.com>
2026-08-27 22:58:37 -0400

    add temporary svg assets for different buildings

---
+++ apps/frontend/src/components/game-boards/frontiers/RoadAsset.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const RoadAsset = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 580 67" {...props}>
+    
+    <g strokeLinecap="round">
+      <g transform="translate(10.01 10) rotate(0 279.995 23.5)" fillRule="evenodd">
+        
+        <path
+          d="M0 0 L559.98 0 L559.99 46.99 L0 47 L0 0"
+          stroke="none"
+          fill="inherit"
+        />
+      </g>
+    </g>
+
+    <g strokeLinecap="round" transform="translate(10 10) rotate(0 280 23.5)">
+      <path
+        d="M-1.18 0.41 C151.1 -2.44, 303.68 -2.76, 561.28 0.09 M0.16 -0.48 C222.98 2.77, 448.01 2.26, 559.58 0.12 M557.51 0.24 C558.99 12.87, 562.5 24.12, 559.27 45.52 M559.95 1.79 C560.29 15.31, 558.9 28.71, 560.6 47.08 M561.49 48.59 C361.7 51.07, 159.08 51.27, -1.45 48.17 M560.61 47.12 C439.7 51.7, 319.97 50.78, 0.48 47.77 M0.02 44.52 C2.41 38.86, 2.21 22, -1.32 0.73 M-0.11 48.05 C0.01 32.32, -0.97 16.77, 1.7 1.9"
+        stroke="currentColor"
+        strokeWidth="10"
+        fill="none"
+      />
+    </g>
+  </svg>
+);
+
+export default RoadAsset;
